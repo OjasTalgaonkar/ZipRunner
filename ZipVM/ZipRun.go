3b881486fa9dadc753c9eb0baf9b66d91795bc59
Ojas <ojasat5@gmail.com>
2025-02-20 15:56:40 -0500

    feat(ZipVM) : started the ziprunner translator

---
+++ ZipVM/ZipRun.go
@@ -0,0 +1,19 @@
+package zipvm
+
+import (
+	"fmt"
+	"os"
+)
+
+func RuntimeDecoder()
+
+func execZpr(inputFile string) {
+
+}
+
+func main() {
+	inputFile := "input.zpr"
+
+	execZpr(inputFile)
+
+}
